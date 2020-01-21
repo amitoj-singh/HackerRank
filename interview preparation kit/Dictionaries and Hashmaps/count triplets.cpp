ab30b28f669174ef7215bc018e84ac2d469b2e69
amitoj-singh <31106921+amitoj-singh@users.noreply.github.com>
2020-01-21 20:34:50 +0530

    Create count triplets.cpp

---
+++ interview preparation kit/Dictionaries and Hashmaps/count triplets.cpp
@@ -0,0 +1,106 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+// Complete the countTriplets function below.
+long countTriplets(vector<long> arr, long r) {
+
+    map<long, int> left, right;
+    long ans = 0, left_count, right_count;
+
+    for (int i = 0; i < arr.size(); i++) {
+        right[arr[i]]++;
+    }
+
+    for (int i = 0; i < arr.size(); i++) {
+        left_count = 0; right_count = 0;        
+        
+        if (arr[i]%r == 0)
+            left_count = left[arr[i]/r];
+
+        right[arr[i]]--; // removing count of elements from right hashmap as we move ahead in the array
+        right_count = right[arr[i]*r]; // keeping this statement here ensures smooth working when r=1
+        ans += left_count * right_count; // updating the answer
+        left[arr[i]]++; // adding count of elements to left hashmap as we move ahead in the array
+    }
+
+    return ans;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string nr_temp;
+    getline(cin, nr_temp);
+
+    vector<string> nr = split(rtrim(nr_temp));
+
+    int n = stoi(nr[0]);
+
+    long r = stol(nr[1]);
+
+    string arr_temp_temp;
+    getline(cin, arr_temp_temp);
+
+    vector<string> arr_temp = split(rtrim(arr_temp_temp));
+
+    vector<long> arr(n);
+
+    for (int i = 0; i < n; i++) {
+        long arr_item = stol(arr_temp[i]);
+
+        arr[i] = arr_item;
+    }
+
+    long ans = countTriplets(arr, r);
+
+    fout << ans << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
