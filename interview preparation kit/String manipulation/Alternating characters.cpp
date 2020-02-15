3010cbc02fe6bf2251911369d9b55e909a75984a
amitoj-singh <31106921+amitoj-singh@users.noreply.github.com>
2020-02-15 21:29:13 +0530

    Create Alternating characters.cpp

---
+++ interview preparation kit/String manipulation/Alternating characters.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Complete the alternatingCharacters function below.
+int alternatingCharacters(string s) {
+    int del_count = 0, length = s.length();
+
+    for (int i = 1; i < length; i++) {
+        if (s[i] == s[i-1])
+            del_count++;
+    }
+
+    return del_count;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int q;
+    cin >> q;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    for (int q_itr = 0; q_itr < q; q_itr++) {
+        string s;
+        getline(cin, s);
+
+        int result = alternatingCharacters(s);
+
+        fout << result << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
