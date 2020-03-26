b0947df394f093881b1ab6579b74ed5543d1e69a
amitoj-singh <31106921+amitoj-singh@users.noreply.github.com>
2020-03-26 21:51:14 +0530

    Create Sparse Arrays.cpp

---
+++ Data Structures/Arrays/Sparse Arrays.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Complete the matchingStrings function below.
+vector<int> matchingStrings(vector<string> strings, vector<string> queries) {
+    vector<int> result;
+    unordered_map<string, int> freqCounter;
+    int n = strings.size(), q = queries.size();
+    for (int i = 0; i < n; i++)
+        freqCounter[strings[i]]++;
+    for (int i = 0; i < q; i++) {
+        if (freqCounter[queries[i]] > 0) 
+            result.push_back(freqCounter[queries[i]]);
+        else
+            result.push_back(0);
+    }
+    return result;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int strings_count;
+    cin >> strings_count;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    vector<string> strings(strings_count);
+
+    for (int i = 0; i < strings_count; i++) {
+        string strings_item;
+        getline(cin, strings_item);
+
+        strings[i] = strings_item;
+    }
+
+    int queries_count;
+    cin >> queries_count;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    vector<string> queries(queries_count);
+
+    for (int i = 0; i < queries_count; i++) {
+        string queries_item;
+        getline(cin, queries_item);
+
+        queries[i] = queries_item;
+    }
+
+    vector<int> res = matchingStrings(strings, queries);
+
+    for (int i = 0; i < res.size(); i++) {
+        fout << res[i];
+
+        if (i != res.size() - 1) {
+            fout << "\n";
+        }
+    }
+
+    fout << "\n";
+
+    fout.close();
+
+    return 0;
+}
